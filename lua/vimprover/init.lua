0b948328bba3fca00bc8d048ace1383c19640b68
Andor <juuso.melentjeff@gmail.com>
2026-03-17 21:21:37 +0200

    feat: log keypresses to a list

--- lua/vimprover/init.lua
+++ lua/vimprover/init.lua
@@ -1,18 +1,26 @@
 local M = {}
 
 M.vimprover_on = false
+M.key_presses = {}
+
+local ns_id = vim.api.nvim_create_namespace("vimprover")
 
 function M.log_key_presses(key, typed)
   if M.vimprover_on then
     print(vim.fn.keytrans(typed))
+    if vim.fn.keytrans(typed) then
+      table.insert(M.key_presses, vim.fn.keytrans(typed))
+    end
   end
 end
 
 function M.toggle_vimprover()
   if M.vimprover_on then
     M.vimprover_on = false
+    vim.print(M.key_presses)
   else
     M.vimprover_on = true
+    M.key_presses = {}
     vim.on_key(M.log_key_presses)
   end
 end
